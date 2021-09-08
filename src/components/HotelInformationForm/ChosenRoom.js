29537b78434a63a086dc2772a8cc20b243c92cd4
Pedro Henrique <phcatanduba@gmail.com>
2021-09-08 18:36:09 -0300

    style: change room button

--- src/components/HotelInformationForm/ChosenRoom.js
+++ src/components/HotelInformationForm/ChosenRoom.js
@@ -1,23 +1,27 @@
 import styled from "styled-components";
+import Button from "../Form/Button";
 
 export default function ChosenRoom({ room }) {
   return (
-    <HotelChoice chosenHotel={false}>
-      <img alt={room.hotel} src={room.hotelImage} />
+    <div>
+      <HotelChoice chosenHotel={false}>
+        <img alt={room.hotel} src={room.hotelImage} />
 
-      <h1>{room.hotel}</h1>
-      <p>Quarto reservado:</p>
-      <span>
-        {room.number} ({room.type})
-      </span>
+        <h1>{room.hotel}</h1>
+        <p>Quarto reservado:</p>
+        <span>
+          {room.number} ({room.type})
+        </span>
 
-      <p>Pessoas no seu quarto</p>
-      <span>
-        {room.available - 1 !== 0
-          ? `Você e mais ${room.available - 1}`
-          : "Você"}
-      </span>
-    </HotelChoice>
+        <p>Pessoas no seu quarto</p>
+        <span>
+          {room.available - 1 !== 0
+            ? `Você e mais ${room.available - 1}`
+            : "Você"}
+        </span>
+      </HotelChoice>
+      <Button>Trocar de quarto</Button>
+    </div>
   );
 }
 
