d057974899bf0dd0c44fcf5a47f1841675cd5a52
André Carmo <andre_de11@msn.com>
2021-09-09 00:28:47 -0300

    fix: moving context and correcting misspeling

--- src/pages/Dashboard/Activities/index.js
+++ src/pages/Dashboard/Activities/index.js
@@ -2,9 +2,9 @@ import { Switch, useRouteMatch } from "react-router-dom";
 
 import ConditionalRoute from "../../../components/Router/ConditionalRoute";
 
-import TicketContext, { TicketProvider } from "../../../contexts/TicketContext";
+import TicketContext from "../../../contexts/TicketContext";
 
-import UnathourizedToChoose from "../../../components/Activities/UnathourizedToChoose";
+import UnathorizedToChoose from "../../../components/Activities/UnathorizedToChoose";
 import { useContext } from "react";
 import ScheduleActivities from "../../../components/Activities/ScheduleActivities";
 
@@ -12,38 +12,42 @@ export default function Activities() {
   const match = useRouteMatch();
 
   return (
-    <TicketProvider>
-      <Switch>
-        <ConditionalRoute
-          check={ensureTicketIsNotPayed}
-          path={`${match.path}/unauthorized`}
-          exact
-        >
-          <UnathourizedToChoose />
-        </ConditionalRoute>
-        <ConditionalRoute
-          check={ensureTicketIsPresencialModality}
-          path={`${match.path}`}
-          exact
-        >
-          <ScheduleActivities />
-        </ConditionalRoute>
-      </Switch>
-    </TicketProvider>
+    <Switch>
+      <ConditionalRoute
+        check={ensureTicketIsNotPayed}
+        path={`${match.path}/unauthorized`}
+        exact
+      >
+        <UnathorizedToChoose />
+      </ConditionalRoute>
+      <ConditionalRoute
+        check={ensureTicketIsPresentialModality}
+        path={`${match.path}`}
+        exact
+      >
+        <ScheduleActivities />
+      </ConditionalRoute>
+    </Switch>
   );
 }
 
 function ensureTicketIsNotPayed() {
   const { ticketData } = useContext(TicketContext);
   return [
-    { to: "/dashboard/activities", check: () => !(ticketData?.isPaid && !ticketData?.isOnline) },
+    {
+      to: "/dashboard/activities",
+      check: () => !(ticketData?.isPaid && !ticketData?.isOnline),
+    },
   ];
 }
 
-function ensureTicketIsPresencialModality() {
+function ensureTicketIsPresentialModality() {
   const { ticketData } = useContext(TicketContext);
 
   return [
-    { to: "/dashboard/activities/unauthorized", check: () => !(!(ticketData?.isPaid) || (ticketData?.isOnline)) }
+    {
+      to: "/dashboard/activities/unauthorized",
+      check: () => !(!ticketData?.isPaid || ticketData?.isOnline),
+    },
   ];
 }
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -3,6 +3,7 @@ import { Switch, Route, Redirect, useRouteMatch } from "react-router-dom";
 import styled from "styled-components";
 
 import EventInfoContext from "../../contexts/EventInfoContext";
+import { TicketProvider } from "../../contexts/TicketContext";
 
 import NavigationBar from "../../components/Dashboard/NavigationBar";
 
@@ -22,31 +23,33 @@ export default function Dashboard() {
       <NavigationBar />
 
       <Container>
-        <Switch>
-          <Route path={`${match.path}/subscription`} exact>
-            <FillSubscription />
-          </Route>
+        <TicketProvider>
+          <Switch>
+            <Route path={`${match.path}/subscription`} exact>
+              <FillSubscription />
+            </Route>
 
-          <Route path={`${match.path}/payment`}>
-            <Payment />
-          </Route>
+            <Route path={`${match.path}/payment`}>
+              <Payment />
+            </Route>
 
-          <Route path={`${match.path}/hotel`} exact>
-            <Hotel />
-          </Route>
+            <Route path={`${match.path}/hotel`} exact>
+              <Hotel />
+            </Route>
 
-          <Route path={`${match.path}/activities`} >
-            <Activities />
-          </Route>
+            <Route path={`${match.path}/activities`}>
+              <Activities />
+            </Route>
 
-          <Route path={`${match.path}/certificate`} exact>
-            <Certificate />
-          </Route>
+            <Route path={`${match.path}/certificate`} exact>
+              <Certificate />
+            </Route>
 
-          <Route path={`${match.path}/`}>
-            <Redirect to={`${match.url}/subscription`} />
-          </Route>
-        </Switch>
+            <Route path={`${match.path}/`}>
+              <Redirect to={`${match.url}/subscription`} />
+            </Route>
+          </Switch>
+        </TicketProvider>
       </Container>
     </DashboardLayout>
   );
