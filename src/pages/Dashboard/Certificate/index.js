bd455f6f71294d5c520bfdd3d0286cb2b95d1cd0
Giovanna Niemeyer <giovanna.niemeyer@gmail.com>
2021-09-16 14:57:38 -0300

    feat: message about certification is adjusted accordingly

--- src/pages/Dashboard/Certificate/index.js
+++ src/pages/Dashboard/Certificate/index.js
@@ -1,3 +1,31 @@
+import styled from "styled-components";
+import EventInfoContext from "../../../contexts/EventInfoContext";
+import { useContext } from "react";
+
 export default function Certificate() {
-  return "Certificado: Em breve!";
+  const { eventInfo } = useContext(EventInfoContext);
+
+  const today = new Date(Date.now());
+  const endDate = new Date(eventInfo.endDate);
+  return (
+    <Message>
+      {endDate > today ? 
+        <h1>O certificado só será disponível após o evento</h1> :
+        <h1>O certificado ficará disponível em breve!</h1>     
+      }
+    </Message>
+  );
 }
+
+const Message = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  h1 {
+    color: #8e8e8e;
+    width: 510px;
+    font-size: 20px;
+    text-align: center;
+  }
+`;
